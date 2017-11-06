e456a1840be69a4e9eddfd911881702dcec441d2
Henry Swaffield <hhswaff@gmail.com>
2017-11-06 14:38:14 -0500

    Hans will replace read_csv()

--- kmeans.py
+++ kmeans.py
@@ -1,10 +1,18 @@
 # Henry Swaffield & Hans Goudey - CS451 - Fall 2017
 
-import csv
+import numpy as np
 
+# lists, with an entry for each training example:
+# X[m] - each a 16d input vector
+# C[m] - a number, j, which cluster the example is closest to
+
+# mu[k] - 16d vector, which countries correspond to the cluster, K
 
 def read_csv():
-    print("hi")
+    data = np.genfromtxt('country.csv', delimiter=",", skip_header=1, usecols=(range(1, 17)))
+    print(data)
+    print(data[1])
+    print(data.shape)
 
 
 def main():
