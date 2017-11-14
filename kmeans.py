6641d38a72bb20119ae430e8debc2c6ff07babbe
Henry Swaffield <hhswaff@gmail.com>
2017-11-14 16:47:04 -0500

    sort of working now, needs to be cleaned... stupid squared error function was wrong...

--- kmeans.py
+++ kmeans.py
@@ -23,7 +23,7 @@ def squared_distance(v1, v2):
     assert (len(v1) == len(v2)), "Vectors are not the same length.\nv1: %r\nv2: %r" % (v1, v2)
     output = 0
     for i in range(len(v1)):
-        output += v1[i] * v2[i]
+        output += (v1[i] - v2[i])**2
     return output
     
 def find_nearest_centroid(xi, mu):
@@ -34,40 +34,59 @@ def get_centroids(x, mu):
     return [find_nearest_centroid(x[i], mu) for i in range(len(x))]
 
 def cost(x, c, mu):
-    print('c:', c)
-    print('mu:', mu)
     return np.mean([squared_distance(x[i], mu[c[i]]) for i in range(len(x))])
 
 
 def kmeans(x, k):
-    print('X:', x)
     mu = x[:k]
     print('MU: ', mu)
     lastCost = 99999999999999999999  #cost(x, get_centroids(x, mu), mu)
-    for i in range(0, 500):
+    for iterations in range(0, 500):
         # Cluster Assignment
         c = get_centroids(x, mu)
 
-        # Update Centroids
+        # Update Centroids (loops through k times:)
         for i in range(len(mu)):
-            # New mu[i] is the average of all of the data points assigned to it
-            data_assigned_to_centroid = []
-            for index in c:
-                if index == i:
-                    data_assigned_to_centroid.append(x[index])
 
+            mu_average = np.zeros((16))
+            count = 0
+
+            for index in range(len(c)):
+                if c[index] == i:
+                    # Match:
+                    # data_assigned_to_centroid.append(x[index])
+                    mu_average = mu_average + x[index]
+                    count += 1
 
 
             # ERROR AREA
-            data_assigned_to_centroid = zip(data_assigned_to_centroid)
-            for d in data_assigned_to_centroid:
-                print(d)
-            mu = np.array([np.mean(x) for x in zip(data_assigned_to_centroid)])  # THIS LINE IS PROBABLY CAUSING THE PROBLEM
+            # updating centroids, by averaging:
+
+            # data_assigned_to_centroid = zip(data_assigned_to_centroid)
+
+            # mu_average = np.zeros(shape)
+
+            # for d in data_assigned_to_centroid:
+            #     print(d)
+            #
+            # mu = np.array([np.mean(x) for x in zip(data_assigned_to_centroid)])  # THIS LINE IS PROBABLY CAUSING THE PROBLEM
+
+            mu[i] = (mu_average / count)
+
+            # print('mu_average - after division: ', mu[i])
+
+            # print(mu[i])
+            # print(mu)
+            # print(mu[0].shape)
 
         # Print Cost
+
+
         thisCost = cost(x, c, mu)
+        print(thisCost)
         if thisCost == lastCost:
             break
+        lastCost = thisCost
 
 
 
